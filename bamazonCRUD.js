868529bd1d9edb85ff380724d150cd3aded933e8
Matthew Yokopovich <matthew.yokopovich@gmail.com>
2019-09-08 18:24:42 -0500

    Initialized logic and set up schema for bamazonDB

---
+++ bamazonCRUD.js
@@ -0,0 +1,65 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "052595my",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    init();
+});
+
+function init() {
+    var query = connection.query(
+        'SELECT * FROM products',
+        function (err, res) {
+            if (err) throw err;
+            res.forEach(function (element) {
+                console.log(`Item ID: ${element.item_id}  Product: ${element.product_name}  Price: $${element.price}`);
+            });
+            inquirer.prompt([{
+                    name: "item_choice",
+                    type: "number",
+                    message: "Please enter the ID of the product you would like to purchase."
+                },
+                {
+                    name: "item_quantity",
+                    type: "number",
+                    message: "Please enter the number of that product that you would like to purchase."
+                }
+            ]).then(function (p) {
+                var innerquery = connection.query(
+                    'SELECT * FROM products WHERE item_id = ?', [p.item_choice], function(err, res){
+                        if(err) throw err;
+                        if(res[0].stock_quantity<p.item_quantity){
+                            console.log("Insufficient Quantity!");
+                        } else{
+                            var temp = res[0].stock_quantity - p.item_quantity;
+                            console.log(temp);
+                            var innerestquery = connection.query(
+                                `UPDATE products SET ? WHERE ?`,[{
+                                    stock_quantity: temp
+                                }, {
+                                    item_id: p.item_choice
+                                }]
+                            );
+                        console.log(innerestquery.sql);
+                        }
+                        
+                    })
+            })
+        }
+    );
+}
---
+++ bamazonDB.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+	item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100),
+    department_name VARCHAR(100),
+    stock_quantity INT,
+    price DECIMAL(10, 2) NULL,
+    PRIMARY KEY (item_id)
+);
